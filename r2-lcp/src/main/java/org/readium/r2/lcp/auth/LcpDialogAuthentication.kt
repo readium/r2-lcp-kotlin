7289fd7e46184c8a42a4fd453903066154c8b886
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2021-08-14 16:09:19 -0500

    Fix passphrase screen not showing correct UI

--- r2-lcp/src/main/java/org/readium/r2/lcp/auth/LcpDialogAuthentication.kt
+++ r2-lcp/src/main/java/org/readium/r2/lcp/auth/LcpDialogAuthentication.kt
@@ -14,6 +14,7 @@ import android.net.Uri
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ListPopupWindow
 import android.widget.PopupWindow
@@ -54,7 +55,7 @@ class LcpDialogAuthentication : LcpAuthenticating {
         else null
 
     private suspend fun askPassphrase(license: LcpAuthenticating.AuthenticatedLicense, reason: LcpAuthenticating.AuthenticationReason, sender: Any?): String? {
-        val hostView = (sender as? View) ?: (sender as? Activity)?.findViewById(android.R.id.content) ?: (sender as? Fragment)?.view
+        val hostView = (sender as? View) ?: (sender as? Activity)?.findViewById<ViewGroup>(android.R.id.content)?.getChildAt(0) ?: (sender as? Fragment)?.view
         ?: run {
             Timber.e("No valid [sender] was passed to `LcpDialogAuthentication::retrievePassphrase()`. Make sure it is an Activity, a Fragment or a View.")
             return null
